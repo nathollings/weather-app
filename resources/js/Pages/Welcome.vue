2b283bd3f60e3482103e6af3d32e4fe486be7e15
Nathanael <nathanael@flume.agency>
2022-08-06 22:52:57 +1200

    feat: welcome page & icon \nfor shits and giggles

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -10,21 +10,53 @@ defineProps({
 </script>
 
 <template>
-    <Head title="Welcome" />
 
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-        <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="text-sm text-gray-700 underline">
-                Dashboard
+    <Head title="Nathanael's weather" />
+
+
+
+    <div
+        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div
+            v-if="canLogin"
+            class="hidden right-0 px-6 py-4 sm:block text-center flex"
+        >
+
+            <h1 class="text-sky-600 mb-2 text-4xl">Nathanael's Weather</h1>
+
+            <img
+                class="mx-auto"
+                src="/logo.svg"
+                width="300"
+            >
+
+            <p class="text-sky-600 mb-3 text-2xl whitespace-normal">
+                Register to check the weather from a
+                <span class="italic underline">single</span> week in 2014
+            </p>
+
+            <Link
+                v-if="$page.props.auth.user"
+                :href="route('dashboard')"
+                class="mt-1 text-3xl text-sky-800 underline"
+            >
+            Dashboard
             </Link>
 
             <template v-else>
-                <Link :href="route('login')" class="text-sm text-gray-700 underline">
-                    Log in
+                <Link
+                    :href="route('login')"
+                    class="mt-1 text-3xl text-sky-800 underline"
+                >
+                Log in
                 </Link>
 
-                <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 underline">
-                    Register
+                <Link
+                    v-if="canRegister"
+                    :href="route('register')"
+                    class="ml-4 text-3xl text-sky-800 underline"
+                >
+                Register
                 </Link>
             </template>
         </div>
@@ -32,65 +64,65 @@ defineProps({
 </template>
 
 <style scoped>
-    .bg-gray-100 {
-        background-color: #f7fafc;
-        background-color: rgba(247, 250, 252, var(--tw-bg-opacity));
+.bg-gray-100 {
+    background-color: #f7fafc;
+    background-color: rgba(247, 250, 252, var(--tw-bg-opacity));
+}
+
+.border-gray-200 {
+    border-color: #edf2f7;
+    border-color: rgba(237, 242, 247, var(--tw-border-opacity));
+}
+
+.text-gray-400 {
+    color: #cbd5e0;
+    color: rgba(203, 213, 224, var(--tw-text-opacity));
+}
+
+.text-gray-500 {
+    color: #a0aec0;
+    color: rgba(160, 174, 192, var(--tw-text-opacity));
+}
+
+.text-gray-600 {
+    color: #718096;
+    color: rgba(113, 128, 150, var(--tw-text-opacity));
+}
+
+.text-gray-700 {
+    color: #4a5568;
+    color: rgba(74, 85, 104, var(--tw-text-opacity));
+}
+
+.text-gray-900 {
+    color: #1a202c;
+    color: rgba(26, 32, 44, var(--tw-text-opacity));
+}
+
+@media (prefers-color-scheme: dark) {
+    .dark\:bg-gray-800 {
+        background-color: #2d3748;
+        background-color: rgba(45, 55, 72, var(--tw-bg-opacity));
     }
 
-    .border-gray-200 {
-        border-color: #edf2f7;
-        border-color: rgba(237, 242, 247, var(--tw-border-opacity));
+    .dark\:bg-gray-900 {
+        background-color: #1a202c;
+        background-color: rgba(26, 32, 44, var(--tw-bg-opacity));
     }
 
-    .text-gray-400 {
+    .dark\:border-gray-700 {
+        border-color: #4a5568;
+        border-color: rgba(74, 85, 104, var(--tw-border-opacity));
+    }
+
+    .dark\:text-white {
+        color: #fff;
+        color: rgba(255, 255, 255, var(--tw-text-opacity));
+    }
+
+    .dark\:text-gray-400 {
         color: #cbd5e0;
         color: rgba(203, 213, 224, var(--tw-text-opacity));
     }
-
-    .text-gray-500 {
-        color: #a0aec0;
-        color: rgba(160, 174, 192, var(--tw-text-opacity));
-    }
-
-    .text-gray-600 {
-        color: #718096;
-        color: rgba(113, 128, 150, var(--tw-text-opacity));
-    }
-
-    .text-gray-700 {
-        color: #4a5568;
-        color: rgba(74, 85, 104, var(--tw-text-opacity));
-    }
-
-    .text-gray-900 {
-        color: #1a202c;
-        color: rgba(26, 32, 44, var(--tw-text-opacity));
-    }
-
-    @media (prefers-color-scheme: dark) {
-        .dark\:bg-gray-800 {
-            background-color: #2d3748;
-            background-color: rgba(45, 55, 72, var(--tw-bg-opacity));
-        }
-
-        .dark\:bg-gray-900 {
-            background-color: #1a202c;
-            background-color: rgba(26, 32, 44, var(--tw-bg-opacity));
-        }
-
-        .dark\:border-gray-700 {
-            border-color: #4a5568;
-            border-color: rgba(74, 85, 104, var(--tw-border-opacity));
-        }
-
-        .dark\:text-white {
-            color: #fff;
-            color: rgba(255, 255, 255, var(--tw-text-opacity));
-        }
-
-        .dark\:text-gray-400 {
-            color: #cbd5e0;
-            color: rgba(203, 213, 224, var(--tw-text-opacity));
-        }
-    }
+}
 </style>
